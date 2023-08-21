e003acb940f0dd87d98e6b7de4e2f1f7f378ec19
Martiniano <bianchimartiniano@gmail.com>
2023-08-21 20:41:43 -0300

    estilos añadidos

--- src/app.js
+++ src/app.js
@@ -13,3 +13,11 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/home.html'))
 })
+
+app.get('/register.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/register.html'))
+})
+
+app.get('/login.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/login.html'))
+})
